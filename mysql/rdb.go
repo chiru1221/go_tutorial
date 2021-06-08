180ead19f8c3f3df2340b0df66052bbc17ab40a5
chiru1221 <takuro1221.it@gmail.com>
2021-06-09 07:24:36 +0900

    src

--- mysql/rdb.go
+++ mysql/rdb.go
@@ -18,4 +18,34 @@ func main() {
 	}
 
 	defer db.Close()
+
+	stmt, err := db.Prepare("insert into todo values(?,?,?,?)")
+	if err != nil{
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec("2", "test", "6/8", "6/8")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	var id int
+	var task string
+	rows, err := db.Query("select id, task from todo")
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next(){
+		err := rows.Scan(&id, &task)
+		if err != nil{
+			log.Fatal(err)
+		}
+		log.Println(id, task)
+	}
+	err = rows.Err()
+	if err != nil{
+		log.Fatal(err)
+	}
+
 }
